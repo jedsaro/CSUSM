3ae7c0cf4d29202b840ee94d07e4d564270a7e3f
Jesus Sanchez <33048003+jedsaro@users.noreply.github.com>
2022-03-22 23:35:43 -0700

    ui testing

--- CS351/Project2/turtleVisitor.py
+++ CS351/Project2/turtleVisitor.py
@@ -7,6 +7,9 @@ else:
 
 # This class defines a complete generic visitor for a parse tree produced by turtleParser.
 
+import turtle
+tutu = turtle.Turtle()
+
 class turtleVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by turtleParser#start.
@@ -16,11 +19,28 @@ class turtleVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by turtleParser#drawlineExpr.
     def visitDrawlineExpr(self, ctx:turtleParser.DrawlineExprContext):
+        target_x    = int(ctx.x_cord.text)
+        target_y    = int(ctx.y_cord.text)
+        cur_pos     = tutu.pos()
+
+        if target_x > cur_pos[0]:
+            tutu.right( target_x - cur_pos[0])
+        else:
+            tutu.left( cur_pos[0] - target_x)
+
+        if target_y > cur_pos[1]:
+            tutu.forward( target_y - cur_pos[1])
+        else:
+            tutu.backward( cur_pos[1] - target_y)
+
+    
+
         return self.visitChildren(ctx)
 
 
     # Visit a parse tree produced by turtleParser#printlineExpr.
     def visitPrintlineExpr(self, ctx:turtleParser.PrintlineExprContext):
+        print(f"Draw line to x={ctx.x_cord.text} y={ctx.y_cord.text}")
         return self.visitChildren(ctx)
 
 
